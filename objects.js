ebc60ea6dc3a84c13f9e7d51f667acb32e381928
amrudees@yahoo.com <amrudees@yahoo.com>
2018-05-12 21:13:05 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -10,3 +10,7 @@ function updatePlaylist(playlist,artist,songtitle){
   playlist[artist]=songtitle
   return playlist
 }
+function removeFromPlaylist(playlist,artist){
+  delete playlist[artist]
+  return playlist
+}
