b133be424ebda79d237bf5f56b9ca6d19c642436
amrudees@yahoo.com <amrudees@yahoo.com>
2018-05-12 21:05:57 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,7 @@
+var playlist = { 
+    artist1: ["song1","song2"],
+    artist2: ["song1","song2"],
+    artist3: ["song1","song2"],
+    artist4: ["song1","song2"],
+    artist5: ["song1","song2"],
+ };
